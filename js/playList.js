a5dc739e560a7fdb1a63e145c0637243c9ad5293
MARS <Sergei-6@bk.ru>
2022-08-04 00:02:49 +0300

    fix: fix minor bag's

--- js/playList.js
+++ js/playList.js
@@ -6,22 +6,22 @@ const playList = [
     },
     {
         title: 'River Flows In You',
-        src: 'momentum/assets/sounds/River_Flows_In_You.mp3',
+        src: 'assets/sounds/River_Flows_In_You.mp3',
         duration: '03:50'
     },
     {
         title: 'Summer Wind',
-        src: './momentum/assets/sounds/Summer_Wind.mp3',
+        src: 'assets/sounds/Summer_Wind.mp3',
         duration: '01:50'
     },
     {
         title: 'Subsurface',
-        src: '../momentum/assets/sounds/Subsurface.mp3',
+        src: 'assets/sounds/Subsurface.mp3',
         duration: '01:50'
     },
     {
         title: 'Chameleon',
-        src: '.../assets/sounds/Aqua_Caelestis.mp3',
+        src: 'assets/sounds/Aqua_Caelestis.mp3',
         duration: '01:50'
     }
 ];
