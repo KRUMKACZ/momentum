4e64714fad9144e23c9b177e04acb0247e84ec37
MARS <Sergei-6@bk.ru>
2022-08-06 22:05:57 +0300

    feat: add progress bar and volume controls

--- js/playList.js
+++ js/playList.js
@@ -2,27 +2,18 @@ const playList = [
     {
         title: 'Aqua Caelestis',
         src: 'assets/sounds/Aqua_Caelestis.mp3',
-        duration: '00:58'
-    },
-    {
-        title: 'River Flows In You',
-        src: 'assets/sounds/River_Flows_In_You.mp3',
-        duration: '03:50'
     },
     {
         title: 'Summer Wind',
         src: 'assets/sounds/Summer_Wind.mp3',
-        duration: '01:50'
     },
     {
         title: 'Subsurface',
         src: 'assets/sounds/Subsurface.mp3',
-        duration: '01:50'
     },
     {
         title: 'Chameleon',
         src: 'assets/sounds/Chameleon.mp3',
-        duration: '01:50'
     }
 ];
 export default playList;
