f10d5fca3ec7eb4033cc78513c7361c2031d84c7
MARS <Sergei-6@bk.ru>
2022-08-14 23:24:05 +0300

    feat: add slider and fix bag's

--- js/allScript.js
+++ js/allScript.js
@@ -13,8 +13,12 @@ function getLocalStorage() {
     // Выводим данные если они сохранены и включены в настройках
     if (localStorage.getItem('userName') && localStorage.getItem('switchGreeting') == 1) {
         userName.value = localStorage.getItem('userName');
+    } else if (!localStorage.getItem('userName') && localStorage.getItem('switchGreeting') == 1) {
+        let languagePlaceholder = localStorage.getItem('switchLanguage');
+        languagePlaceholder == 'en' ? userName.placeholder = '[Enter name]' : userName.placeholder = '[Введите имя]';
     }
 }
 window.addEventListener('load', getLocalStorage);
 
+changeLanguageClick.addEventListener('click', getLocalStorage);
 
--- js/clock.js
+++ js/clock.js
@@ -9,8 +9,8 @@ changeLanguageClock();
 const arrDayEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const arrDayRu = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-const arrWelcomeEn = ['Good morning,', 'Good afternoon,', 'Good evening,', 'Good night,'];
-const arrWelcomeRu = ['Доброе утро,', 'Добрый день,', 'Добрый вечер,', 'Доброй ночи,'];
+const arrWelcomeEn = ['Good morning', 'Good afternoon', 'Good evening', 'Good night'];
+const arrWelcomeRu = ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Доброй ночи'];
 
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
@@ -29,8 +29,11 @@ function showDate(getTimeAndDate) {
     date.textContent = nameDay + ', ' + currentDate; // Выводим дату на страницу приложения
 }
 
-function getTimeOfDay(getTimeAndDate) {
-    const hours = getTimeAndDate.getHours();
+
+let hours; // Глобальное время
+// Возвращаем приветствие в зависимости от времени суток
+function getGreeting(getTimeAndDate) {
+    hours = getTimeAndDate.getHours();
     let arrWelcome = [];
     language == 'en-En' ? arrWelcome = arrWelcomeEn : arrWelcome = arrWelcomeRu;
     if (hours >= 6 && hours < 12) {
@@ -53,8 +56,8 @@ function showTime() {
     time.textContent = currentTime; // Выводим время на страницу приложения
     showDate(getTimeAndDate); // Вызываем функцию даты и дня недели
 
-    const timeOfDay = getTimeOfDay(getTimeAndDate); // Возвращаем время суток
-    greeting.textContent = timeOfDay; // Выводим время на страницу приложения
+    const greetingTime = getGreeting(getTimeAndDate); // Получаем приветствие в зависимости от времени суток
+    greeting.textContent = greetingTime + ','; // Выводим приветствие на страницу приложения
 
     setTimeout(showTime, 1000); // Рекурсивно вызываем функцию (для обновления времени)
 }
