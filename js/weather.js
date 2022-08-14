a653bfee4846de032929bfc8abe28ed006a1694b
MARS <Sergei-6@bk.ru>
2022-08-14 15:32:05 +0300

    fix: add translate city weather

--- js/weather.js
+++ js/weather.js
@@ -3,22 +3,20 @@ const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
-const city = document.querySelector('.city');
 const weatherContent = document.querySelector('.weather-content');
+let city = document.querySelector('.city');
 
 const weatherSettingHidden = document.querySelector('.weather');
 localStorage.getItem('switchWeather') == 0 ? weatherSettingHidden.classList.add('hidden') : weatherSettingHidden.classList.remove('hidden');
 
-
 // Сохранение введенного имени в Local Storage
 function setCityWeather() {
-    let city = document.querySelector('.city'); // Находим элемент ввода имени
     localStorage.setItem('city', city.value); // Сохраняем введенные данные
 }
 window.addEventListener('beforeunload', setCityWeather);
 
+// Получение города из Local Storage
 function getCityWeather() {
-    let city = document.querySelector('.city'); // Находим элемент ввода имени
     if (localStorage.getItem('city')) { // Выводим данные если они сохранены 
         city.value = localStorage.getItem('city');
         return city.value;
@@ -27,19 +25,19 @@ function getCityWeather() {
         return 'Minsk';
     }
 }
-window.addEventListener('load', getCityWeather);
+//window.addEventListener('load', getCityWeather);
 
 
 async function getWeather() {
     let keyAPI = '5c10fcf2508db1dee5a0feb5c3fd0b43';
     let lang = localStorage.getItem('switchLanguage');
-    let city = getCityWeather(); // Устанавливаем город погоды из localStorage
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&appid=${keyAPI}&units=metric`;
+    let cityWeather = getCityWeather(); // Устанавливаем город погоды из localStorage
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityWeather}&lang=${lang}&appid=${keyAPI}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
-    //console.log(data.cod);
-    //console.log(data);
+
     if (data.cod == 200) {
+        city.value = data.name;
         weatherIcon.className = 'weather-icon owf';
         weatherIcon.classList.add(`owf-${data.weather[0].id}`);
         temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
