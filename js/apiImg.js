7d065a572227f03fe6acb4ce2251918ea8815f20
MARS <Sergei-6@bk.ru>
2022-08-17 01:07:50 +0300

    feat: add API img

--- js/apiImg.js
+++ js/apiImg.js
@@ -3,6 +3,7 @@ let inputTagsImgBlock = document.querySelector('.inputTagsImg');
 let tagInputImg = document.querySelector('.tagInputImg');
 let imageSource = localStorage.getItem('imageSource');
 let tagsImg = localStorage.getItem('tagsImg');
+let unsplasTagImg;
 
 function setImageSourseDefault() {
     let radioDefault;
@@ -27,21 +28,56 @@ showInputTagsImgBlock();
 function setTagsImg() {
     localStorage.setItem('tagsImg', tagInputImg.value); // Сохраняем введенные данные  
 }
+
 window.addEventListener('beforeunload', setTagsImg);
+tagInputImg.addEventListener('keyup', event => {
+    if (event.code === 'Enter') {
+        setTagsImg();
+        let imageSource = localStorage.getItem('imageSource');
+        if (imageSource == 'UnsplashAPI') { getImageUnsplash(); }
+        if (imageSource == 'FlickrAPI') { flickrAPI(); }
+    }
+});
+
+
+
 
 
 function getTagsImg() {
     tagsImg = localStorage.getItem('tagsImg');
     tagInputImg.value = tagsImg;
 }
-if (tagsImg != null) {
+
+if (tagsImg) {
     getTagsImg();
 }
+
+function getTagImgLocalStorege() {
+    if (localStorage.getItem('tagsImg')) {
+        unsplasTagImg = localStorage.getItem('tagsImg');
+    } else {
+        unsplasTagImg = 'morning';
+        localStorage.setItem('tagsImg', unsplasTagImg);
+        getTagsImg();
+    }
+}
+getTagImgLocalStorege();
+
 
 apiImgRadio.onclick = function (event) {
     if (event.target.value) {
         setLocalStorageSettings('imageSource', event.target.value);
     }
-    event.target.value == 'UnsplashAPI' || event.target.value == 'FlickrAPI' ? inputTagsImgBlock.classList.remove('hidden') : inputTagsImgBlock.classList.add('hidden');
-    // getTagsImg();
+    if (event.target.value == 'UnsplashAPI' || event.target.value == 'FlickrAPI') {
+        inputTagsImgBlock.classList.remove('hidden');
+        if (event.target.value == 'UnsplashAPI') {
+            getImageUnsplash();
+        } else if (event.target.value == 'FlickrAPI') {
+            flickrAPI();
+        }
+    } else {
+        inputTagsImgBlock.classList.add('hidden');
+        setBg();
+    }
+    getTagsImg();
 };
--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,14 @@ const body = document.body;
 const slidePrev = document.querySelector('.slide-prev');
 const slideNext = document.querySelector('.slide-next');
 
+const unsplashKey = 'gJkChubIgNt9kRxbmBLesSbAbBwRckJjILDU94tDLzk';
+const orientationImg = 'landscape';
+
+const flickrKey = 'e93ccbe4fc4a84fdf59b46f82b6d84cb';
+const sizeImgFlickr = 'url_h';
+
+const imgSourgeAPI = localStorage.getItem('imageSource');
+
 let randomNum;
 function getRandomNum(min, max) {
     return randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -24,25 +32,6 @@ function getImg() {
 }
 getImg();
 
-function getSlidePrev() {
-    if (randomNum == 1) {
-        randomNum = 20;
-        setBg();
-    } else {
-        randomNum--;
-        setBg();
-    }
-}
-
-function getSlideNext() {
-    if (randomNum == 20) {
-        randomNum = 1;
-        setBg();
-    } else {
-        randomNum++;
-        setBg();
-    }
-}
 
 function setBg() {
     const img = new Image();
@@ -51,8 +40,98 @@ function setBg() {
         document.body.style.backgroundImage = `url(${img.src})`;
     });
 }
-setBg();
 
+async function getImageUnsplash() {
+    const img = new Image();
+    const url = `https://api.unsplash.com/photos/random?orientation=${orientationImg}&query=${unsplasTagImg}&client_id=${unsplashKey}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    img.src = data.urls.regular;
+    img.addEventListener('load', function () {
+        document.body.style.backgroundImage = `url(${img.src})`;
+        document.body.style.backgroundSize = 'cover';
+    });
+}
+
+let countFlickr = 0;
+async function flickrAPI() {
+    const img = new Image();
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrKey}&tags=${unsplasTagImg}&extras=${sizeImgFlickr}&format=json&nojsoncallback=1`;
+    const res = await fetch(url);
+    const data = await res.json();
+    img.src = data.photos.photo[countFlickr].url_h;
+    img.addEventListener('load', function () {
+        document.body.style.backgroundImage = `url(${img.src})`;
+        document.body.style.backgroundSize = 'cover';
+    });
+}
+
+function getSlidePrev() {
+    let imgSourgeAPI = localStorage.getItem('imageSource');
+    if (imgSourgeAPI == 'GitHub') {
+        if (randomNum == 1) {
+            randomNum = 20;
+            setBg();
+        } else {
+            randomNum--;
+            setBg();
+        }
+    }
+
+    if (imgSourgeAPI == 'UnsplashAPI') {
+        getImageUnsplash();
+    }
+
+    if (imgSourgeAPI == 'FlickrAPI') {
+        if (countFlickr < 1) {
+            countFlickr = 50;
+        } else {
+            countFlickr--;
+        }
+        flickrAPI(countFlickr);
+    }
+}
+
+function getSlideNext() {
+    let imgSourgeAPI = localStorage.getItem('imageSource');
+
+    if (imgSourgeAPI == 'GitHub') {
+        if (randomNum == 20) {
+            randomNum = 1;
+            setBg();
+        } else {
+            randomNum++;
+            setBg();
+        }
+    }
+
+    if (imgSourgeAPI == 'UnsplashAPI') {
+        getImageUnsplash();
+    }
+
+    if (imgSourgeAPI == 'FlickrAPI') {
+        if (countFlickr > 50) {
+            countFlickr = 0;
+        } else {
+            countFlickr++;
+        }
+        flickrAPI(countFlickr);
+    }
+
+}
+
+
+switch (imgSourgeAPI) {
+    case 'GitHub':
+        setBg();
+        break;
+    case 'UnsplashAPI':
+        getImageUnsplash();
+        break;
+    case 'FlickrAPI':
+        flickrAPI();
+        break;
+}
 
 
 slidePrev.onclick = () => {
@@ -62,8 +141,3 @@ slidePrev.onclick = () => {
 slideNext.onclick = () => {
     getSlideNext();
 };
-
-
-getSlidePrev();
-getSlideNext();
-
